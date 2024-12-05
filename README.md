1ce0ca36f4963a596589f8b4bab2aaa0d1f62b5b
fxdlz <1970475803@qq.com>
2024-12-06 01:00:21 +0800

    1 day

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gee-rpc
+极客兔兔gee-rpc项目学习
---
+++ server.go
@@ -0,0 +1,135 @@
+package geerpc
+
+import (
+	"encoding/json"
+	"fmt"
+	"gee-rpc/codec"
+	"io"
+	"log"
+	"net"
+	"reflect"
+	"sync"
+)
+
+const MagicNumber = 0x117c00
+
+type Option struct {
+	MagicNumber int
+	CodecType   codec.Type
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+var DefaultServer = NewServer()
+
+func (s *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error:", err)
+			return
+		}
+		go s.ServeConn(conn)
+	}
+}
+
+func Accept(lis net.Listener) {
+	DefaultServer.Accept(lis)
+}
+
+func (s *Server) ServeConn(conn io.ReadWriteCloser) {
+	defer func() { _ = conn.Close() }()
+	var opt Option
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server: option error: ", err)
+		return
+	}
+	if opt.MagicNumber != MagicNumber {
+		log.Printf("rpc server: invalid magic number %x", opt.MagicNumber)
+		return
+	}
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		log.Printf("rpc server: invalid codec type %s", opt.CodecType)
+		return
+	}
+	s.serveCodec(f(conn))
+}
+
+type request struct {
+	h            *codec.Header
+	argv, replyv reflect.Value
+}
+
+var invalidRequest = struct{}{}
+
+func (s *Server) serveCodec(cc codec.Codec) {
+	sending := new(sync.Mutex)
+	wg := new(sync.WaitGroup)
+	for {
+		req, err := s.readRequest(cc)
+		if err != nil {
+			if req == nil {
+				break
+			}
+			req.h.Error = err.Error()
+			s.sendResponse(cc, req.h, invalidRequest, sending)
+		}
+		wg.Add(1)
+		go s.handleRequest(cc, req, sending, wg)
+	}
+	wg.Wait()
+	_ = cc.Close()
+}
+
+func (s *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
+	var h codec.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error:", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+
+func (s *Server) readRequest(cc codec.Codec) (*request, error) {
+	h, err := s.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{
+		h: h,
+	}
+	req.argv = reflect.New(reflect.TypeOf(""))
+	err = cc.ReadBody(req.argv.Interface())
+	if err != nil {
+		log.Println("rpc server: read argv err:", err)
+		return req, err
+	}
+	return req, nil
+}
+
+func (s *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
+	sending.Lock()
+	defer sending.Unlock()
+	if err := cc.Write(h, body); err != nil {
+		log.Println("rpc server: write response error:", err)
+	}
+}
+
+func (s *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Println(req.h, req.argv.Elem())
+	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq))
+	s.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+}
